d8ada205edf8ebbb8d2eefcd020965e212f914f2
Vedant Puri <vedantpuri@gmail.com>
2024-12-09 15:59:11 -0500

    Update diffusion.py

--- fastdiff/diffusion.py
+++ fastdiff/diffusion.py
@@ -28,8 +28,8 @@ class Diffusion(nn.Module):
 
         self.mode = mode
         self.model = model
-        self.schedule_type = schedule_type #new
-        self.schedule_params = schedule_params #new
+        self.schedule_type = schedule_type
+        self.schedule_params = schedule_params
         self.lossfun = nn.MSELoss()
         self.log_max_steps = 8 # 2 ** N
 
@@ -38,7 +38,6 @@ class Diffusion(nn.Module):
     
     schedule_types = ['cosine', 'laplace', 'cauchy', 'cosine_shifted', 'cosine_scaled', 'exponential', 'quadratic']
     
-    #new
     def cosine_schedule(self,t):
         return np.cos((1 - t) * math.pi / 2)
     def exponential_schedule(self,t,beta=1.0):
@@ -51,7 +50,6 @@ class Diffusion(nn.Module):
 #         return mu - b * np.sign(0.5 - t) * np.log(1 - 2 * np.abs(t - 0.5))
         return mu - b * np.sign(0.5 - t) * np.log(np.maximum(1 - 2 * np.abs(t - 0.5), 1e-6))
 
-
     def cauchy_schedule(self, t, mu=0, gamma=1):
         return mu + gamma * np.tan(math.pi / 2 * (1 - 2 * t))
 
@@ -60,7 +58,6 @@ class Diffusion(nn.Module):
 
     def cosine_scaled_schedule(self, t, s=1):
         return 2 / s * np.log(np.tan(math.pi * t / 2))
-    #new
 
     def get_schedule(self, t):
         if self.schedule_type == 'cosine':
@@ -99,50 +96,13 @@ class Diffusion(nn.Module):
 
         return xt
     
-    def predict_velocity(self, xt, alpha, N):
-    
-        batch_size = xt.size(0)  # batch size
-        d = 1 / N 
-        dd = torch.full((batch_size,), d, device=xt.device)
-
+    def predict_velocity(self, xt, t, d):
         if self.mode == 0:  # Flow Matching
-            return self.model(xt, torch.full((batch_size,), alpha, device=xt.device))
+            return self.model(xt, t)
+        else:  # Shortcut Model
+            return self.model(xt, t, d)
 
-        else:  #Shortcut Model
-            return self.model(xt, torch.full((batch_size,), alpha, device=xt.device), dd)
-
-#     @torch.no_grad()
-#     def sample(self, x0, N):
-#         xt = x0
-
-#         for t in range(N):
-# #             alpha_t = self.cosine_schedule(t / N) #alphat
-#             alpha_t = self.cosine_schedule(torch.tensor(t / N, dtype=torch.float32, device=xt.device))
-
-            
-#             alpha_t_next = self.cosine_schedule((t + 1) / N) #alphat+1
-
-#             alpha_t_half = self.cosine_schedule((t + 0.5) / N) #alphat+0.5
-
-#             # Velocity at alpha_t => D_theta
-#             vt = self.predict_velocity(xt, alpha_t, N)
-
-#             # Halfway => x_alpha_t0.5 = x_alpha_t + () D_theta()
-#             xt_half = xt + (alpha_t_half - alpha_t) * vt
-
-#             # Velocity at halfway => D_theta_half
-#             vt_half = self.predict_velocity(xt_half, alpha_t_half, N)
-
-#             xt = xt + (alpha_t_next - alpha_t) * vt_half
-
-#             # else:
-#             #     vt = self.predict_velocity(xt, alpha_t, N)
-#             #     xt = xt + (alpha_t_next - alpha_t) * vt
-
-#         return xt
-
-
-    def _sample(self, x1):
+    def _train_sample(self, x1):
         B = x1.size(0)
         device = x1.device
 
@@ -154,7 +114,6 @@ class Diffusion(nn.Module):
      
         return x0, xt, tt, dd
     
-
     #==================================#
     # losses
     #==================================#
@@ -169,7 +128,7 @@ class Diffusion(nn.Module):
         return loss_FM + loss_CM
 
     def loss_CM(self, x1):
-        x0, xt, tt, dd = self._sample(x1)
+        x0, xt, tt, dd = self._train_sample(x1)
 
         # vt_XY: v(t + X * d, Y * d)
 
@@ -184,7 +143,7 @@ class Diffusion(nn.Module):
         return self.lossfun(vt_02, v_avg.detach())
 
     def loss_FM(self, x1, use_d: bool):
-        x0, xt, tt, dd = self._sample(x1)
+        x0, xt, tt, dd = self._train_sample(x1)
 
         if use_d:
             vt = self.model(xt, tt, dd)
